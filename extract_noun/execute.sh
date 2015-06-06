8f4446e9a3aed34994b05a4316e9dfc5b1ed9c0b
cnu-taeguk <xornrbboy@gmail.com>
2015-06-07 03:32:00 +0900

    clean up~

--- extract_noun/execute.sh
+++
@@ -1,1 +0,0 @@
-java -cp ./class/:lib/komoran-2.4-e.jar:lib/shineware-common-2.0.jar:lib/shineware-ds-1.0.jar ds_project/Test
--- web_parsing/zzc001/b.py
+++
@@ -1,7 +0,0 @@
-import time
-import os
-
-while True:
-	os.system('python a.py')
-	time.sleep(60)
-
